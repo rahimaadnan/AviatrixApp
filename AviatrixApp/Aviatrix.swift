bc36f68cf7a411cec044ac9448745a1333f2e0f3
Apple <apple@Apples-MacBook-Pro-35.local>
2019-06-19 09:01:09 -0700

    complete iteration 0

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -8,10 +8,18 @@
 
 import Foundation
 
+
 class Aviatrix {
+    var running = false
+    var author : String
+    init(authorName : String)
+    {
+        author = authorName
+    }
     
     func start() -> Bool {
-        return true
+        running = true
+        return running
     }
     
     func refuel() {
